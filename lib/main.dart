ba7562911f4829a2e65b0c4390fe6a0978480429
natoriouskpk <TristanGrobasch@googlemail.com>
2021-12-04 17:38:29 +0100

    AppBar with specific text arrow_back_button Image of the job Title of the job image_carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ScreenJobdetails',
       theme: ThemeData(
+        fontFamily: 'Roboto',
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'ScreenJobdetails'),
+      home: const MyHomePage(title: 'Pflege der Grünflächen'),
     );
   }
 }
@@ -39,27 +41,76 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text(widget.title)),
-        leading: IconButton(
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            widget.title,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+          /*leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
-        )
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        )*/ // Back-Arrow
+        ),
+        body: Column(
+          children: [
+            Image.network(
+              'https://www.rose-galabau.de/wp-content/uploads/2021/10/Rose-landschaftsbau-gruenflaechenpflege-gewerbliche-landschaftsgaertner-pflasterarbeiten.jp_.jpg',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Pflege der Grünflächen",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 200.0,
+                  aspectRatio: 16/9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  //onPageChanged: callbackFunction,
+                  scrollDirection: Axis.horizontal,
+              ),
+              items: [1,2,3,4,5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 3.0, vertical: 15.0),
+                        decoration: BoxDecoration(
+                            color: Colors.black
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                            child: Text('Tätigkeit $i', style: TextStyle(fontSize: 16.0, color: Colors.white),),
+                          ),
+                        ),
+                    );
+                  },
+                );
+              }).toList(),
             ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
