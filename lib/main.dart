9765eb80cd435024d15f80a54ce77a1ff0c2ebe7
natoriouskpk <TristanGrobasch@googlemail.com>
2021-12-05 12:38:52 +0100

    - AppBar with specific text - arrow_back_button ----------------------------- - Image of the job - Title of the job - image_carousel - job description - work activities as circles with a blue-grey background ----------------------------- - graduation text - circle_avatar with gradutation icon in it

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -57,9 +58,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: ListView(
           children: [
-              Image.network(
-                'https://www.rose-galabau.de/wp-content/uploads/2021/10/Rose-landschaftsbau-gruenflaechenpflege-gewerbliche-landschaftsgaertner-pflasterarbeiten.jp_.jpg',
-              ),
+            Image.network(
+              'https://www.rose-galabau.de/wp-content/uploads/2021/10/Rose-landschaftsbau-gruenflaechenpflege-gewerbliche-landschaftsgaertner-pflasterarbeiten.jp_.jpg',
+            ),
             Padding(
               padding: EdgeInsets.fromLTRB(44, 10, 0, 0),
               child: Align(
@@ -73,46 +74,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 200.0,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.8,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeCenterPage: true,
-                  //onPageChanged: callbackFunction,
-                  scrollDirection: Axis.horizontal,
-                ),
-                items: [1, 2, 3, 4, 5].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 3.0, vertical: 15.0),
-                        decoration: BoxDecoration(color: Colors.black),
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                            child: Text(
-                              'Tätigkeit $i',
-                              style:
-                                  TextStyle(fontSize: 16.0, color: Colors.white),
-                            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                //onPageChanged: callbackFunction,
+                scrollDirection: Axis.horizontal,
+              ),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 3.0, vertical: 15.0),
+                      decoration: BoxDecoration(color: Colors.black),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                          child: Text(
+                            'Tätigkeit $i',
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.white),
                           ),
                         ),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
             Padding(
               padding: EdgeInsets.fromLTRB(44, 10, 44, 0),
               child: Align(
@@ -129,59 +130,137 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
               child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 100,
-                  color: Colors.blueGrey,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(43, 10, 15, 15),
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                color: Colors.blueGrey,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(27, 10, 15, 15),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 55,
+                            height: 55,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Image(
+                                image: AssetImage('icons/comments-solid.png'),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                            child: Text(
+                              "Fähigkeit 1",
+                              style: TextStyle(),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(15, 10, 17, 15),
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 55,
+                            height: 55,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Image(
+                                image: AssetImage('icons/walking-solid.png'),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                            child: Text(
+                              "Fähigkeit 2",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 55,
+                            height: 55,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Image(
+                                image: AssetImage(
+                                    'icons/assistive-listening-systems-solid.png'),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                            child: Text(
+                              "Fähigkeit 3",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 10, 17, 15),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 55,
+                            height: 55,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Image(
+                                image: AssetImage('icons/swimmer-solid.png'),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                            child: Text(
+                              "Fähigkeit 4",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(55, 30, 0, 0),
+              child: Row(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Schulabschluss",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(65, 0, 40, 0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.blueGrey,
+                        minRadius: 50,
+                        maxRadius: 50,
+                        child: Image(
+                          image: AssetImage('icons/user-graduate-solid.png'),
+                        ),
+                      ),
+                  ),
+                ],
+              ),
+            )
           ],
         ));
   }
